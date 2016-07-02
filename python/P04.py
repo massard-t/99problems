066a9941e9a09d476e7595f677273244c3d599c3
Theo Massard <massar_t@etna-alternance.net>
2016-07-02 16:36:34 +0200

    Added P04 Tested P04

---
+++ python/P04.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+# 1.04 (*) Find the number of elements of a list.
+
+def func(data):
+    return len(data)
--- python/tests.py
+++ python/tests.py
@@ -1,6 +1,7 @@
 import P01
 import P02
 import P03
+import P04
 
 
 def test_p01_with_list_returns_last_one():
@@ -25,3 +26,12 @@ def test_p03_with_list_returns_kth_elem_one():
 
 def test_p03_with_list_returns_kth_elem_two():
     assert 0 == P03.func([0, 3, -3, 1, 3], 1)
+
+
+def test_p04_with_list_returns_length_one():
+    assert 3 == P04.func([0, 9, 10])
+
+
+def test_p04_with_list_returns_length_two():
+    assert 5 == P04.func([5, 5, 5, 5, 4])
+
